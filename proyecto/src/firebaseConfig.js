8f5879f9cc048bb169adafac1f1599f7d1c3b09d
juanbuitri <buitragodiego2004@gmail.com>
2025-05-24 00:58:09 +0000

    segundo

--- proyecto/src/firebaseConfig.js
+++ proyecto/src/firebaseConfig.js
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth'; 
 import { getFirestore } from 'firebase/firestore'; 
 const firebaseConfig = { 
- apiKey: "aqui su codigo", 
- authDomain: "aqui su codigo", 
- projectId: "aqui su codigo", 
- storageBucket: "aqui su codigo", 
- messagingSenderId: "aqui su codigo", 
- appId: "aqui su codigo" 
+ apiKey: "AIzaSyBsO5YkmJV-HRNyV8sTkUdYMFcm39jwsiY",
+  authDomain: "pokeapi2-37992.firebaseapp.com",
+  projectId: "pokeapi2-37992",
+  storageBucket: "pokeapi2-37992.firebasestorage.app",
+  messagingSenderId: "901790018781",
+  appId: "1:901790018781:web:9bc674b4c17f6d96eabf34"
+ 
 }; 
 const app = initializeApp(firebaseConfig); 
 const auth = getAuth(app); 
